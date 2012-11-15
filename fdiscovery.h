314e193ad04e56a5d12252347f6e72b24e8ae000
Laila Atrmouh <laila.atrmouh@gmail.com>
2012-11-15 11:21:39 -0500

    discovery functions

---
+++ fdiscovery.h
@@ -0,0 +1,18 @@
+/**
+* @file FDiscovery.h
+* @brief Contains all the methods related to the discovery of hosts
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <bluetooth/bluetooth.h>
+#include <bluetooth/hci.h>
+#include <bluetooth/hci_lib.h>
+
+/**
+ * Discovering hosts reachable
+ */
+void discovery();
+
---
+++ main.c
@@ -0,0 +1,7 @@
+#include "fdiscovery.h"
+
+int main() {
+	printf("- FileStix -");
+	discovery();
+	return EXIT_SUCCESS;
+}
