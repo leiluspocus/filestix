03614579c8601d10e5e07809e2290da6efd7b3ed
Laila Atrmouh <laila.atrmouh@gmail.com>
2012-11-16 15:09:15 -0500

    first attempt of user interface (just for the bluetooth tests)

--- main.c
+++ main.c
@@ -1,7 +1,23 @@
 #include "fdiscovery.h"
 
 int main() {
+	int choice = 0;
+	
 	printf("- FileStix -");
-	discovery();
+	printf("Press 1 to scan, 2 to send, -1 to exit");
+	scanf("%d", &choice);
+	
+	switch(choice) {
+		case 1:
+			discovery();	
+			break;
+		case 2:
+			send_file();
+			break;
+		default:
+			printf("Unknown choice…");
+			break;
+	}
+	
 	return EXIT_SUCCESS;
 }
